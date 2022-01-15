87fb6a0c1a48c5928be080773439fa442fc646d4
rajkumar888 <Soma@rajeev67>
2022-01-16 00:31:13 +0800

    first commit

---
+++ src/test/java/com/demo/TestJenkinsDemo.java
@@ -0,0 +1,11 @@
+package com.demo;
+
+import org.testng.annotations.Test;
+
+public class TestJenkinsDemo {
+
+	@Test
+	public void test_001(){
+		System.out.println("Welcome To Jenkins Demo.....");
+	}
+}
